c52e1bd0d399350d9defc0ef709a7f9e6cf0c495
BT_4N2_08 <tzhao9389@bths.edu>
2022-11-09 12:30:45 -0500

    change

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,2 +1,32 @@
 public class Schedule {
+
+    private int event;
+
+    public Schedule(){
+        this.event=event;
+
+    }
+    public int dayPick(){
+        int day=(int)(Math.random()*7)+1;
+    }
+    public boolean morning(){
+        if (event<=3){
+            return true;
+
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean noon(){
+        if (event<=5){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean afternoon(){
+
+    }
 }
